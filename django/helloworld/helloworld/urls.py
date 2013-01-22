1536f6de8d18e72521388e6f4296325a3699326d
dawndiy <chenglu1990@gmail.com>
2013-01-23 03:13:52 +0800

    add url of /time to the demo

--- django/helloworld/helloworld/urls.py
+++ django/helloworld/helloworld/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-	url(r'^hello/$', 'helloworld.views.hello')
+	url(r'^hello/$', 'helloworld.views.hello'),
+	url(r'^time/$', 'helloworld.views.current_datetime')
 )
--- django/helloworld/helloworld/views.py
+++ django/helloworld/helloworld/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
 	return HttpResponse("Hello world!")
 
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body><h2>It is now %s</h2></body></html>" % now
+	return HttpResponse(html)
