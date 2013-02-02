b9afd4bdd3a6a4c6ed9361c0d7d76e476511d206
dawndiy <chenglu1990@gmail.com>
2013-02-02 15:35:48 +0800

    添加 Action 中 GET/POST 传参示例

--- javaweb/Struts2/src/com/dawndiy/action/LoginAction.java
+++ javaweb/Struts2/src/com/dawndiy/action/LoginAction.java
@@ -10,15 +10,17 @@ public class LoginAction extends ActionSupport {
 
 	private String username;
 
+	// 用于在页面上显示
 	public String getUsername() {
 		return username;
 	}
 
+	// 可接收来自 Action 的传参
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
-	
+	// Action 默认执行方法
 	public String execute(){
 
 		SimpleDateFormat tempDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -30,9 +32,15 @@ public class LoginAction extends ActionSupport {
 		return "in";
 	}
 	
+	// 自定义的 Action 方法
 	public String admin(){
 		System.out.println("use admin()");
 		username = "admin";
+		return "in";
+	}
+	
+	// 通过 GET/POST 方式获取请求参数
+	public String getParameters(){
 		return "in";
 	}
 	
