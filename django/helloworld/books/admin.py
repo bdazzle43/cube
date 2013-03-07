e7470557a6abed5c2d645d5a6873bf6a368b7815
dawndiy <chenglu1990@gmail.com>
2013-03-07 21:02:19 +0800

    update admin.py

--- django/helloworld/books/admin.py
+++ django/helloworld/books/admin.py
@@ -5,9 +5,14 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
     search_fields = ('first_name', 'last_name')
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
 
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 
 
