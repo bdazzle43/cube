12714c44dac80ab3f420d96297b3729481a68687
dawndiy <chenglu1990@gmail.com>
2013-03-07 18:18:32 +0800

    update the display of Author

--- django/helloworld/books/admin.py
+++ django/helloworld/books/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from books.models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
+
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
+
+
