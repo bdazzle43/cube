66d307e69f04be47af2f71d8f3e5653205ab7aa8
DawnDIY <chenglu1990@gmail.com>
2013-05-28 15:53:24 +0800

    A Tour of Go

---
+++ go/go-tour/45.switch-with-no-condition.go
@@ -0,0 +1,26 @@
+/*
+ 没有条件的 switch
+ =================
+
+ 没有条件的 switch 同 switch true 一样。
+ 这一构造使得可以用更清晰的形式来编写长的 if-then-else 链。
+*/
+
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func main() {
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("Good morning!")
+    case t.Hour() < 17:
+        fmt.Println("Good afternoon.")
+    default:
+        fmt.Println("Good evening.")
+    }
+}
