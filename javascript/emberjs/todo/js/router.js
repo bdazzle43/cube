74e286ee5aec6f5061e911a3acf73521391424a8
DawnDIY <chenglu1990@gmail.com>
2013-08-16 00:02:01 +0800

    EmberJS-Tutorial-Todo Displaying Model Data

--- javascript/emberjs/todo/js/router.js
+++ javascript/emberjs/todo/js/router.js
@@ -2,3 +2,9 @@
 Todos.Router.map(function () {
     this.resource('todos', { path: '/' });
 });
+
+Todos.TodosRoute = Ember.Route.extend({
+    model: function () {
+        return Todos.Todo.find();
+    }
+});
